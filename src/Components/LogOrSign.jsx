fc191ece8757dab747668889b159d19cf2cf5da2
yousofSayed <yousef.sayed1231@gmail.com>
2023-11-29 22:33:56 +0200

    cm-6

--- src/Components/LogOrSign.jsx
+++ src/Components/LogOrSign.jsx
@@ -10,8 +10,8 @@ function LogOrSign() {
     }
 
     return ( 
-        <section id="logOrSign" className="w-full h-full fixed flex justify-center items-center blur-c">
-            <button className="fixed right-9 top-7 w-8 h-8 rounded-full bg-gray-950 hover:text-cyan-400 transition" onClick={hide}><i className="fa-solid fa-x"></i></button>
+        <section id="logOrSign" className="w-full h-full absolute flex justify-center items-center rounded-lg">
+            {/* <button className="fixed right-9 top-7 w-8 h-8 rounded-full bg-gray-950 hover:text-cyan-400 transition" onClick={hide}><i className="fa-solid fa-x"></i></button> */}
             <article className="p-4 bg-gray-900">
                 <p className="p-4 font-bold">You Should Login Or Signup</p>
                 <div className="flex justify-evenly items-center"><Link className={styles.btn} to={`login`}>Login</Link> | <Link className={styles.btn} to={`signup`}>Signup</Link></div>
