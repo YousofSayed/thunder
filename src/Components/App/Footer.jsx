8127ba994ea118e9e2b6276f48debe9b618ec3c3
yousofSayed <yousef.sayed1231@gmail.com>
2023-12-20 18:15:17 +0200

    cm-24

--- src/Components/App/Footer.jsx
+++ src/Components/App/Footer.jsx
@@ -7,29 +7,29 @@ function Footer() {
     useEffect(() => {
         const route = location.pathname.replace('/', '')
         if (!route) {
-            $(`#routes #home`).classList.remove('text-shadow');
-            $(`#routes #home`).classList.add('text-shadow', 'text-cyan-400')
+            // $(`#routes #home`).classList.remove('text-shadow','text-black','dark:text-white');
+            $(`#routes #home`).classList.add('text-[12px]', 'ring-2' ,'bg-gray-950')
             return;
         };
-        $a('#routes a').forEach(a => a.classList.remove('text-shadow', 'text-cyan-400'));
-        $(`#routes #${route}`).classList.add('text-shadow', 'text-cyan-400')
+        $a('#routes a').forEach(a => a.classList.remove('text-shadow','text-[12px]', 'ring-2' ,'bg-gray-950'));
+        $(`#routes #${route}`).classList.add('text-shadow','text-[12px]', 'ring-2' ,'bg-gray-950')
     })
 
     const handleRoutes = (ev) => {
         $a('#routes a').forEach(a => {
-            a.classList.remove('text-shadow', 'text-cyan-400')
-            ev.currentTarget.classList.add('text-shadow', 'text-cyan-400')
+            a.classList.remove('text-shadow','text-[12px]', 'ring-2' ,'bg-gray-950')
+            ev.currentTarget.classList.add('text-shadow','text-[12px]', 'ring-2' ,'bg-gray-950')
         });
     };
 
     return (
         <footer className="w-full h-[45px] flex  justify-center px-2">
             <ul id="routes" className="container bg-[#eee] dark:bg-gray-900 rounded-lg p-2 flex items-center justify-between">
-                <Link id="profile" className="text-black dark:text-white" to={'/profile'} onClick={handleRoutes}><i className="fa-solid fa-user cursor-pointer"></i></Link>
-                <Link id="settings" className="text-black dark:text-white" to={'/settings'} onClick={handleRoutes}><i className="fa-solid fa-gear cursor-pointer"></i></Link>
-                <Link id="home" className="text-black dark:text-white" to={'/'} onClick={handleRoutes}><i className="fa-solid fa-home cursor-pointer "></i></Link>
-                <Link id="bookmarks" className="text-black dark:text-white" to={'/bookmarks'} onClick={handleRoutes}><i className="fa-solid fa-bookmark cursor-pointer"></i></Link>
-                <Link id="search" className="text-black dark:text-white" to={'/search'} onClick={handleRoutes}><i className="fa-solid fa-search cursor-pointer"></i></Link>
+                <Link id="profile" className="text-cyan-400 w-[25px] h-[25px] flex items-center justify-center  rounded-full" to={'/profile'} onClick={handleRoutes}><i className="fa-solid fa-user cursor-pointer"></i></Link>
+                <Link id="bookmarks" className="text-cyan-400 w-[25px] h-[25px] flex items-center justify-center  rounded-full" to={'/bookmarks'} onClick={handleRoutes}><i className="fa-solid fa-bookmark cursor-pointer"></i></Link>
+                <Link id="home" className="text-cyan-400 w-[25px] h-[25px] flex items-center justify-center  rounded-full" to={'/'} onClick={handleRoutes}><i className="fa-solid  fa-home cursor-pointer "></i></Link>
+                <Link id="settings" className="text-cyan-400 w-[25px] h-[25px] flex items-center justify-center  rounded-full" to={'/settings'} onClick={handleRoutes}><i className="fa-solid fa-gear cursor-pointer"></i></Link>
+                <Link id="search" className="text-cyan-400 w-[25px] h-[25px] flex items-center justify-center  rounded-full" to={'/search'} onClick={handleRoutes}><i className="fa-solid fa-search cursor-pointer"></i></Link>
             </ul>
         </footer>
     );
